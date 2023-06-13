ca12eb4ce1d9404bf31e3e38c0e552c0a7ced69f
Daníel Adams <me@dandabs.io>
2023-06-13 19:53:32 +0100

    customize certificate

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
 
+const inquirer = require('inquirer');
+
 const fs = require("fs");
 const path = require("path");
 
@@ -16,19 +18,102 @@ const doc = new Docxtemplater(zip, {
     linebreaks: true,
 });
 
-doc.render({
-    shareholder_name_lc: "Daniel Adams",
-    shareholder_name_uc: "DANIEL ADAMS",
-    shareholder_address_uc: "103 LISNAMURRIKIN ROAD, BROUGHSHANE, BALLYMENA, NORTHERN IRELAND, BT42 4PP",
-    date: new Date().toLocaleString('en-GB', { dateStyle: "short" }),
-    no: String(-1),
-    quantity: String(-1),
-    class: "ORDINARY",
-});
+console.log("This utility will walk you through creating a share certificate for IglooCode Ltd.");
+console.log("The generated share certificate must be printed and signed by two directors and a witness.")
+console.log("")
+console.log("Press ^C at any time to quit.")
 
-const buf = doc.getZip().generate({
-    type: "nodebuffer",
-    compression: "DEFLATE",
-});
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Shareholder name:',
+        default: require("os").userInfo().username
+    },
+    {
+        type: 'input',
+        name: 'address',
+        message: 'Shareholder address:',
+    },
+    {
+        type: 'input',
+        name: 'quantity',
+        message: 'Number of shares:',
+        validate(value) {
+          const valid = !isNaN(parseFloat(value));
+          return valid || 'Please enter a number';
+        },
+        filter: Number,
+      },
+      {
+        type: 'list',
+        name: 'class',
+        message: 'Share class:',
+        choices: [
+            {
+                key: 'o',
+                name: 'Ordinary',
+                value: 'ORDINARY'
+            },
+            {
+                key: 'p',
+                name: 'Preference',
+                value: 'PREFERENCE'
+            },
+            {
+                key: 'd',
+                name: 'Deferred',
+                value: 'DEFERRED'
+            },
+            {
+                key: 'r',
+                name: 'Redeemable',
+                value: 'REDEEMABLE'
+            }
+        ],
+        default: 'ORDINARY'
+      },
+      {
+        type: 'input',
+        name: 'no',
+        message: 'Certificate no:',
+        validate(value) {
+          const valid = !isNaN(parseFloat(value));
+          return valid || 'Please enter a number';
+        },
+        filter: Number,
+      },
+]).then(a => {
 
-fs.writeFileSync(path.resolve(__dirname, "output.docx"), buf);
+    doc.render({
+        shareholder_name_lc: a.name,
+        shareholder_name_uc: a.name.toUpperCase(),
+        shareholder_address_uc: a.address.toUpperCase(),
+        date: new Date().toLocaleString('en-GB', { dateStyle: "short" }),
+        no: String(a.no),
+        quantity: String(a.quantity),
+        class: a.class,
+    });
+
+    const buf = doc.getZip().generate({
+        type: "nodebuffer",
+        compression: "DEFLATE",
+    });
+
+    const rnd = (Math.random() + 1).toString(36).substring(7);
+    
+    fs.writeFileSync(path.resolve(require("os").tmpdir(), `${rnd}.docx`), buf);
+
+    var exec = require('child_process').exec;
+    exec(getCommandLine() + ' ' + path.resolve(require("os").tmpdir(), `${rnd}.docx`));
+
+})
+
+function getCommandLine() {
+    switch (process.platform) { 
+       case 'darwin' : return 'open';
+       case 'win32' : return 'start';
+       case 'win64' : return 'start';
+       default : return 'xdg-open';
+    }
+ }
